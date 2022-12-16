e5ddc3c74a27b560bedaf1599139fa7177e8ae58
Hígor Lapa <higor.lapa@cloudwalk.io>
2022-12-16 18:53:53 -0300

    fix: don't call if on ios

--- packages/plugins/rudder_plugin/lib/RudderController.dart
+++ packages/plugins/rudder_plugin/lib/RudderController.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:rudder_sdk_flutter_platform_interface/platform.dart';
 import 'package:rudder_sdk_flutter_platform_interface/rudder_sdk_platform.dart';
 
@@ -12,58 +14,94 @@ class RudderController {
 
   void initialize(String writeKey,
       {RudderConfig? config, RudderOption? options}) {
+    if (Platform.isIOS) {
+      return;
+    }
     RudderSdkPlatform.instance
         .initialize(writeKey, config: config, options: options);
   }
 
   void identify(String userId, {RudderTraits? traits, RudderOption? options}) {
+    if (Platform.isIOS) {
+      return;
+    }
     RudderSdkPlatform.instance
         .identify(userId, traits: traits, options: options);
   }
 
   void track(String eventName,
       {RudderProperty? properties, RudderOption? options}) {
+    if (Platform.isIOS) {
+      return;
+    }
     RudderSdkPlatform.instance
         .track(eventName, properties: properties, options: options);
   }
 
   void screen(String screenName,
       {String? category, RudderProperty? properties, RudderOption? options}) {
+    if (Platform.isIOS) {
+      return;
+    }
     RudderSdkPlatform.instance.screen(screenName,
         category: category, properties: properties, options: options);
   }
 
   void group(String groupId,
       {RudderTraits? groupTraits, RudderOption? options}) {
+    if (Platform.isIOS) {
+      return;
+    }
     RudderSdkPlatform.instance
         .group(groupId, groupTraits: groupTraits, options: options);
   }
 
   void alias(String newId, {RudderOption? options}) {
+    if (Platform.isIOS) {
+      return;
+    }
     RudderSdkPlatform.instance.alias(newId, options: options);
   }
 
   void reset() {
+    if (Platform.isIOS) {
+      return;
+    }
     RudderSdkPlatform.instance.reset();
   }
 
   void optOut(bool optOut) {
+    if (Platform.isIOS) {
+      return;
+    }
     RudderSdkPlatform.instance.optOut(optOut);
   }
 
   void putDeviceToken(String deviceToken) {
+    if (Platform.isIOS) {
+      return;
+    }
     RudderSdkPlatform.instance.putDeviceToken(deviceToken);
   }
 
   void putAdvertisingId(String advertisingId) {
+    if (Platform.isIOS) {
+      return;
+    }
     RudderSdkPlatform.instance.putAdvertisingId(advertisingId);
   }
 
   void putAnonymousId(String anonymousId) {
+    if (Platform.isIOS) {
+      return;
+    }
     RudderSdkPlatform.instance.putAnonymousId(anonymousId);
   }
 
   Future<Map?> getRudderContext() async {
+    if (Platform.isIOS) {
+      return null;
+    }
     return RudderSdkPlatform.instance.getRudderContext();
   }
 }
