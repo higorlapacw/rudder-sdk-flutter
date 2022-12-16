fa77b837b1e1989c255506629e7ba1a94955dab1
Hígor Lapa <higor.lapa@cloudwalk.io>
2022-12-16 18:50:01 -0300

    fix: remove ios implementation

--- packages/plugins/rudder_plugin_ios/lib/rudder_plugin_ios.dart
+++ packages/plugins/rudder_plugin_ios/lib/rudder_plugin_ios.dart
@@ -16,139 +16,43 @@ class RudderSdkFlutterIos extends RudderSdkPlatform {
 
   @override
   void initialize(String writeKey,
-      {RudderConfig? config, RudderOption? options}) {
-    config ??= RudderConfigBuilder().build();
-    Map<String, dynamic> params = {};
-    params['writeKey'] = writeKey;
-    params['config'] = config.toMapMobile();
-    if (options != null) {
-      params['options'] = options.toMap();
-    }
-    _platformChannel.invokeMethod("initializeSDK", params);
-  }
+      {RudderConfig? config, RudderOption? options}) {}
 
   @override
-  void identify(String userId, {RudderTraits? traits, RudderOption? options}) {
-    Map<String, dynamic> params = {};
-
-    params["userId"] = userId;
-
-    if (traits != null) {
-      params["traits"] = traits.traitsMap;
-    }
-
-    if (options != null) {
-      params["options"] = options.toMap();
-    }
-
-    _platformChannel.invokeMethod("identify", params);
-  }
+  void identify(String userId, {RudderTraits? traits, RudderOption? options}) {}
 
   @override
   void track(String eventName,
-      {RudderProperty? properties, RudderOption? options}) {
-    Map<String, dynamic> params = {};
-
-    params["eventName"] = eventName;
-
-    if (properties != null) {
-      params["properties"] = properties.getMap();
-    }
-
-    if (options != null) {
-      params["options"] = options.toMap();
-    }
-
-    _platformChannel.invokeMethod("track", params);
-  }
+      {RudderProperty? properties, RudderOption? options}) {}
 
   @override
   void screen(String screenName,
-      {String? category, RudderProperty? properties, RudderOption? options}) {
-    Map<String, dynamic> params = {};
-
-    params["screenName"] = screenName;
-
-    if (properties != null) {
-      params["properties"] = properties.getMap();
-    }
-
-    if (options != null) {
-      params["options"] = options.toMap();
-    }
-    params["category"] = category;
-    _platformChannel.invokeMethod("screen", params);
-  }
+      {String? category, RudderProperty? properties, RudderOption? options}) {}
 
   @override
   void group(String groupId,
-      {RudderTraits? groupTraits, RudderOption? options}) {
-    Map<String, dynamic> params = {};
-
-    params["groupId"] = groupId;
-
-    if (groupTraits != null) {
-      params["groupTraits"] = groupTraits.traitsMap;
-    }
-
-    if (options != null) {
-      params["options"] = options.toMap();
-    }
-
-    _platformChannel.invokeMethod("group", params);
-  }
+      {RudderTraits? groupTraits, RudderOption? options}) {}
 
   @override
-  void alias(String newId, {RudderOption? options}) {
-    Map<String, dynamic> params = {};
-
-    params["newId"] = newId;
-
-    if (options != null) {
-      params["options"] = options.toMap();
-    }
-
-    _platformChannel.invokeMethod("alias", params);
-  }
+  void alias(String newId, {RudderOption? options}) {}
 
   @override
-  void reset() {
-    _platformChannel.invokeMethod("reset");
-  }
+  void reset() {}
 
   @override
-  void optOut(bool optOut) {
-    Map<String, dynamic> params = {};
-    params["optOut"] = optOut;
-    _platformChannel.invokeMethod("optOut", params);
-  }
+  void optOut(bool optOut) {}
 
   @override
-  void putDeviceToken(String deviceToken) {
-    Map<String, dynamic> params = {};
-
-    params["deviceToken"] = deviceToken;
-    _platformChannel.invokeMethod("putDeviceToken", params);
-  }
+  void putDeviceToken(String deviceToken) {}
 
   @override
-  void putAdvertisingId(String advertisingId) {
-    Map<String, dynamic> params = {};
-
-    params["advertisingId"] = advertisingId;
-    _platformChannel.invokeMethod("putAdvertisingId", params);
-  }
+  void putAdvertisingId(String advertisingId) {}
 
   @override
-  void putAnonymousId(String anonymousId) {
-    Map<String, dynamic> params = {};
-
-    params["anonymousId"] = anonymousId;
-    _platformChannel.invokeMethod("putAnonymousId", params);
-  }
+  void putAnonymousId(String anonymousId) {}
 
   @override
   Future<Map?> getRudderContext() async {
-    return await _platformChannel.invokeMethod("getRudderContext") as Map?;
+    return {};
   }
 }
